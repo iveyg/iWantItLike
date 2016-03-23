2d9fe2ef597a47ba8dcce33b4007f3fbd200f363
iveyg <lovecashpoker@gmail.com>
2016-03-23 23:11:04 +0300

    Bootstap template

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-res.send("Hello, this is our new startup. PUA MAKE IT RAIN mfk");
-//res.render('index', { title: 'Express' });
+//res.send("Hello, this is our new startup. PUA MAKE IT RAIN mfk");
+res.render('index', { title: 'Express' });
+//res.sendFile('../views/front.html', {root: __dirname});
+
 });
 
 module.exports = router;
