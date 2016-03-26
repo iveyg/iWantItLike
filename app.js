92033ea9a9ada1cd97d9c137f1eea64d2c7b3320
iveyg <lovecashpoker@gmail.com>
2016-03-26 22:52:56 +0300

    new release

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var newaccount = require('./routes/newaccount');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));  
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -22,8 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routers 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/newaccount', newaccount);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
