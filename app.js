3ebd67454a5ce93434730ac438b5d839ad873505
Ivan Gorodko <ivan.gorodko@uchi.ru>
2016-04-17 22:47:57 +0300

    new main page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 var newaccount = require('./routes/newaccount');
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routers 
-app.use('/', routes);
+app.use('/', index);
 app.use('/users', users);
 app.use('/newaccount', newaccount);
 
